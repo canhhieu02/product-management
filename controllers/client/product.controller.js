7f810e9f07f9da431737e1de7168e5f0b7cc77f5
canhhieu02 <165915320+canhhieu02@users.noreply.github.com>
2025-07-22 20:55:22 +0700

    tạo danh sách sản phẩm phía client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        //  status: "active",
+        //  deleted: false
+    });
+    console.log(products);
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price-(item.price*item.discountPercentage/100)).toFixed(1);
+        return item;
+    })
+
     res.render("client/pages/products/index", {
-        pageTitle : "Danh sách sản phẩm"
+        pageTitle : "Danh sách sản phẩm",
+        products: newProducts
     });
 }
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 require("dotenv").config(); 
 
+const database = require("./config/database");
+
 const router = require("./router/client/index.route");
+
+database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 router(app);
 
